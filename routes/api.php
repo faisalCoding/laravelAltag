e565e188178381ab93f7aa5b00a5349ba0ab020f
faisal anaya <faisalbakhsh07@gmail.com>
2021-01-28 21:21:24 +0300

    api2

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('info', function () {
+Route::get('/info', function () {
     return response().json([
         name=>'حسام'
     ]);
