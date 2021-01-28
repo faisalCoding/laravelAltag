b9a3dec2da5ec28a2cdfb845fac554e8cab56f6d
faisal anaya <faisalbakhsh07@gmail.com>
2021-01-28 23:00:57 +0300

    api3

--- routes/api.php
+++ routes/api.php
@@ -22,37 +22,37 @@ Route::get('/info', function () {
     return response()->json([
        
             "الاحد 12/3" => [
-              "1" => [
+              1 => [
                 "name" => "خالد",
                 "hfrom" => "القارعة",
                 "hto" => "الليل",
                 "mto" => "الناس",
                 "mfrom" => "المسد",
-                "starsCount" => "3",
-                "list" => "true,true,true",
-                "hasFire" => "true",
+                "starsCount" => 3,
+                "list" => [true,true,true],
+                "hasFire" => true,
               ],
-              "2" => [
+              2 => [
                 "name" => "حسام",
                 "hfrom" => "القارعة",
                 "hto" => "الليل",
                 "mto" => "الناس",
                 "mfrom" => "المسد",
-                "starsCount" => "2",
-                "list" => "true,false,true",
-                "hasFire" => "true",
+                "starsCount" => 2,
+                "list" => [true,false,true],
+                "hasFire" => true,
               ],
             ],
             "الاثنين 12/4" => [
-              "1" => [
+              1 => [
                 "name" => "خالد",
                 "hfrom" => "القارعة",
                 "hto" => "الليل",
                 "mto" => "الناس",
                 "mfrom" => "المسد",
-                "starsCount" => "3",
-                "list" => "false,false,false",
-                "hasFire" => "false",
+                "starsCount" => 3,
+                "list" => [false,false,false],
+                "hasFire" => false,
               ],
             ],
             "الاثلاثاء 12/5" => []
