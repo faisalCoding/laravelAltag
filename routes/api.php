a8037dc5df882436ed4747677c7a87a746d9a500
faisal anaya <faisalbakhsh07@gmail.com>
2021-01-29 14:18:34 +0300

    api4

--- routes/api.php
+++ routes/api.php
@@ -61,44 +61,6 @@ Route::get('/info', function () {
 });
 
 Route::get('/checkUpdate', function () {
-  return md5(response()->json([
-       
-    "الاحد 12/3" => [
-      1 => [
-        "name" => "خالد",
-        "hfrom" => "القارعة",
-        "hto" => "الليل",
-        "mto" => "الناس",
-        "mfrom" => "المسد",
-        "starsCount" => 3,
-        "list" => [true,true,true],
-        "hasFire" => true,
-      ],
-      2 => [
-        "name" => "حسام",
-        "hfrom" => "القارعة",
-        "hto" => "الليل",
-        "mto" => "الناس",
-        "mfrom" => "المسد",
-        "starsCount" => 2,
-        "list" => [true,false,true],
-        "hasFire" => true,
-      ],
-    ],
-    "الاثنين 12/4" => [
-      1 => [
-        "name" => "خالد",
-        "hfrom" => "القارعة",
-        "hto" => "الليل",
-        "mto" => "الناس",
-        "mfrom" => "المسد",
-        "starsCount" => 3,
-        "list" => [false,false,false],
-        "hasFire" => false,
-      ],
-    ],
-    "الاثلاثاء 12/5" => []
-  
-]));
+  return md5('{"الاحد 12/3":{"1":{"name":"خالد","hfrom":"القارعة","hto":"الليل","mto":"الناس","mfrom":"المسد","starsCount":3,"list":[true,true,true],"hasFire":true},"2":{"name":"حسام","hfrom":"القارعة","hto":"الليل","mto":"الناس","mfrom":"المسد","starsCount":2,"list":[true,false,true],"hasFire":true}},"الاثنين 12/4":{"1":{"name":"خالد","hfrom":"القارعة","hto":"الليل","mto":"الناس","mfrom":"المسد","starsCount":3,"list":[false,false,false],"hasFire":false}},"الاثلاثاء 12/5":[]}');
 });
 
