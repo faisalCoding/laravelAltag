80080c16a1fcf621943a75773a3e19a796ac2d5a
faisal anaya <faisalbakhsh07@gmail.com>
2021-01-29 14:23:48 +0300

    api4

--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,47 @@ Route::get('/info', function () {
 });
 
 Route::get('/checkUpdate', function () {
-  return md5('{"الاحد 12/3":{"1":{"name":"خالد","hfrom":"القارعة","hto":"الليل","mto":"الناس","mfrom":"المسد","starsCount":3,"list":[true,true,true],"hasFire":true},"2":{"name":"حسام","hfrom":"القارعة","hto":"الليل","mto":"الناس","mfrom":"المسد","starsCount":2,"list":[true,false,true],"hasFire":true}},"الاثنين 12/4":{"1":{"name":"خالد","hfrom":"القارعة","hto":"الليل","mto":"الناس","mfrom":"المسد","starsCount":3,"list":[false,false,false],"hasFire":false}},"الاثلاثاء 12/5":[]}');
+  $str = response()->json([
+       
+    "الاحد 12/3" => [
+      1 => [
+        "name" => "خالد",
+        "hfrom" => "القارعة",
+        "hto" => "الليل",
+        "mto" => "الناس",
+        "mfrom" => "المسد",
+        "starsCount" => 3,
+        "list" => [true,true,true],
+        "hasFire" => true,
+      ],
+      2 => [
+        "name" => "حسام",
+        "hfrom" => "القارعة",
+        "hto" => "الليل",
+        "mto" => "الناس",
+        "mfrom" => "المسد",
+        "starsCount" => 2,
+        "list" => [true,false,true],
+        "hasFire" => true,
+      ],
+    ],
+    "الاثنين 12/4" => [
+      1 => [
+        "name" => "خالد",
+        "hfrom" => "القارعة",
+        "hto" => "الليل",
+        "mto" => "الناس",
+        "mfrom" => "المسد",
+        "starsCount" => 3,
+        "list" => [false,false,false],
+        "hasFire" => false,
+      ],
+    ],
+    "الاثلاثاء 12/5" => []
+  
+]);
+  $strUtf32 = mb_convert_encoding($str, "UTF-32LE");
+
+  return md5($strUtf32);
 });
 
