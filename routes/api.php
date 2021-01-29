3dd065ab21cc2114b50b8b4f9b7611d6dd73cb91
faisal anaya <faisalbakhsh07@gmail.com>
2021-01-29 17:20:28 +0300

    api4

--- routes/api.php
+++ routes/api.php
@@ -103,8 +103,6 @@ Route::get('/checkUpdate', function () {
   $strUtf32 = mb_convert_encoding($str, "UTF-8");
 
   return  response([ 
-    "md5" => md5($strUtf32) ,
-    "strutf8" => $strUtf32,
     "str" => $str
     ]);
 });
