556d55f3d3e65424683046453edbe46fdadfb8c7
faisal anaya <faisalbakhsh07@gmail.com>
2021-06-04 22:24:20 +0300

    v1

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChartsController;
 use App\Models\Day;
 use App\Models\StudentsState;
+use App\Models\User;
+
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -59,3 +65,29 @@ Route::get('trend', function () {
 });
 
 Route::get('mcount', [ChartsController::class,'fiveTopMaxMStudents']);
+
+Route::post('/sanctum/token', function (Request $request) {
+  $request->validate([
+      'email' => 'required|email',
+      'password' => 'required',
+      'device_name' => 'required',
+  ]);
+
+  $user = User::where('email', $request->email)->first();
+
+  if (! $user || ! Hash::check($request->password, $user->password)) {
+      throw ValidationException::withMessages([
+          'email' => ['The provided credentials are incorrect.'],
+      ]);
+  }
+
+  return $user->createToken($request->device_name)->plainTextToken;
+});
+
+
+Route::middleware('auth:sanctum')->get('/user/revoke', function (Request $request) {
+
+  $user = $request->user;
+  $user->tokens()->delete();
+  return 'tokens are deleted';
+});
